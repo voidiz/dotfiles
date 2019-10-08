5b999aa17b544384ae8b93b78c973f45e3403e9a
void <voidqt@hotmail.com>
2019-10-08 21:26:31 +0200

    separate dark and light colorschemes

--- stowdots.sh
+++ stowdots.sh
@@ -2,15 +2,34 @@
 
 set -eu
 
-pillow=(awesome compton kitty rofi themes systemd spacemacs vim x xresources zsh)
-i3=(compton dunst i3 kitty polybar rofi themes systemd spacemacs vim x xresources zsh)
+pillow=(awesome compton kitty rofi themes
+    systemd vim x xresources zsh
+)
+
+i3=(
+    compton i3 polybar-base rofi-base
+    themes systemd vim x zsh
+)
+
+dark=(
+   dunst-dark polybar-dark rofi-dark xresources-dark 
+)
+
+light=(
+   dunst-light polybar-light rofi-light xresources-light 
+)
 
 name=$1[@]
 wow=("${!name}")
 
 for dot in ${wow[@]}; do
-    stow $dot
+    stow $dot --override=.+
     echo "Symlinking $dot"
 done
+
+if [[ "$1" == "dark" ]] || [[ "$1" == "light" ]]; then
+    xrdb $HOME/.Xresources
+    echo "Set Xresources"
+fi
 
 echo "Done!"
