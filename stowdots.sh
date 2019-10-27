ce23c8c0257781df8716bb3d3027c427d462c224
void <voidqt@hotmail.com>
2019-10-27 03:24:29 +0100

    make stowdots install stow through submodule

--- stowdots.sh
+++ stowdots.sh
@@ -21,15 +21,35 @@ light=(
    vim-light
 )
 
+scriptdir=$(dirname "$(readlink -f "$0")")
+
+# Install stow if it doesn't exist
+if [[ ! -x "$scriptdir/stow-build/bin/stow" ]]; then
+    echo "Installing stow..."
+    cd stow && git init && autoreconf -i 
+    ./configure --prefix=$scriptdir/stow-build
+    GIT_DIR=$scriptdir/.git make install && cd ..
+    echo -e "\nSuccessfully installed stow!\n"
+fi
+
 name=$1[@]
 wow=("${!name}")
 
 for dot in ${wow[@]}; do
-    stow $dot --override=.+
+    stow-build/bin/stow $dot --override=.+
     echo "Symlinking $dot"
 done
 
-apply_xresoures() {
+feh_change_bg() {
+    if [[ -x "$(command -v feh)" ]]; then
+        feh --bg-scale $1
+        echo "Set background"
+    else
+        echo "feh not installed, can't change bg"
+    fi
+}
+
+apply_xresources() {
     if [[ ! -d "$HOME/.vim/bg" ]]; then
         mkdir "$HOME/.vim/bg"
     fi
@@ -42,37 +62,35 @@ apply_xresoures() {
 
     if [[ -n $(pgrep dunst) ]]; then
         killall dunst
+    else
+        echo "Dunst process not found, won't restart"
     fi
 
     # Requires python module neovim-remote
     if [[ -x "$(command -v nvr)" ]] && [[ -n $(pgrep nvim) ]]; then
         nvr --remote-send ":set bg=$1<CR>"
         echo "Changed background of all nvim processes"
+    else
+        echo "Neovim-remote and/or nvim process not found, won't change bg"
     fi
 
     if [[ -n $(pgrep polybar) ]]; then
         polybar-msg cmd restart > /dev/null 2>&1
         echo "Reloaded polybar"
+    else
+        echo "Polybar not found, won't restart"
     fi
 
     i3-msg reload > /dev/null 2>&1
     echo "Reloaded i3"
 }
 
+apply_xresources $1
+
 if [[ "$1" == "dark" ]]; then
-    apply_xresoures $1
-
-    if [[ -x "$(command -v hsetroot)" ]]; then
-        feh --bg-scale $HOME/.config/i3/dark-pink-mountain.jpg
-        echo "Set background"
-    fi
+    feh_change_bg $HOME/.config/i3/dark-pink-mountain.jpg
 elif [[ "$1" == "light" ]]; then
-    apply_xresoures $1
-
-    if [[ -x "$(command -v feh)" ]]; then
-        feh --bg-scale $HOME/.config/i3/pink-mountain.jpg
-        echo "Set background"
-    fi
+    feh_change_bg $HOME/.config/i3/pink-mountain.jpg
 fi
 
 echo "Done!"
