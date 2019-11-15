2349f7c90f7c55039fd9619c2b831ef5f0bb1c9b
voidiz <voidqt@hotmail.com>
2019-11-15 21:22:05 +0100

    add installing packages to setup.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # dotfiles
+contains various personal dotfiles and a script 
+that symlinks dotfiles and installs packages
+
+## script usage
+`./setup.sh i3` - symlinks i3 config dots  
+`./setup.sh dark` - symlinks dark colorscheme  
+`./setup.sh light` - symlinks light colorscheme  
+`./setup.sh packages` - installs arch base/community packages  
+`./setup.sh aur` - installs aur packages (and yay)  
--- setup.sh
+++ setup.sh
@@ -12,30 +12,52 @@ i3=(
 )
 
 dark=(
-   dunst-dark polybar-dark rofi-dark xresources-dark 
-   vim-dark
+    dunst-dark polybar-dark rofi-dark xresources-dark 
+    vim-dark
 )
 
 light=(
-   dunst-light polybar-light rofi-light xresources-light 
-   vim-light
+    dunst-light polybar-light rofi-light xresources-light 
+    vim-light
 )
 
-name=$1[@]
-wow=("${!name}")
+packages="
+    xorg-server xorg-xinit i3-gaps compton dunst i3lock-color
+    noto-fonts rofi
 
-for dot in ${wow[@]}; do
-    stow $dot --override=.+
-    echo "Symlinking $dot"
-done
+    pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol
+
+    bluez bluez-utils netctl ifplugd wpa_supplicant
+
+    curl ffmpeg go nodejs npm python imagemagick neovim 
+    python-pynvim stow wget youtube-dl zsh
+
+    deluge firefox discord telegram-desktop fcitx
+    fcitx-mozc fcitx-im feh flameshot kdeconnect keepassxc
+    mpv mps-youtube streamlink sxiv ttf-hanazono zathura
+    zathura-pdf-mupdf
+"
+
+aur="
+    polybar neovim-remote nnn-git xst-git 
+
+    otf-san-francisco otf-sfmono ttf-font-awesome 
+    ttf-material-design-icons ttf-unifont
+
+    chatterino2-git plex-media-player spicetify-cli spotify
+    ffmpeg-compat-57 dropbox indicator-kdeconnect-git
+"
+
+stow_dots() {
+    for dot in ${list_contents[@]}; do
+        stow $dot --override=.+
+        echo "Symlinking $dot"
+    done
+}
 
 feh_change_bg() {
-    if [[ -x "$(command -v feh)" ]]; then
-        feh --bg-scale $1
-        echo "Set background"
-    else
-        echo "feh not installed, can't change bg"
-    fi
+    feh --bg-scale $1
+    echo "Set background"
 }
 
 apply_xresources() {
@@ -74,11 +96,33 @@ apply_xresources() {
     echo "Reloaded i3"
 }
 
-apply_xresources $1
+install_packages() {
+    if [[ $1 == "packages" ]]; then
+        pacman -S $2 --noconfirm
+    elif [[ $1 == "aur" ]]; then
+        if [[ ! -x "$(command -v yay)" ]]; then
+            git clone https://aur.archlinux.org/yay.git $HOME/yay
+            cd $HOME/yay && makepkg -si --noconfirm
+            rm -rf $HOME/yay
+        fi
+        yay -S $2 --noconfirm
+    fi
+}
 
-if [[ "$1" == "dark" ]]; then
+if [[ $1 != "packages" ]] && [[ $1 != "aur" ]]; then
+    list_name=$1[@]
+    list_contents=${!list_name}
+    stow_dots "$list_contents"
+    apply_xresources $1
+else
+    str_name=$1
+    str_content=${!str_name}
+    install_packages "$str_name" "$str_content"
+fi
+
+if [[ $1 == "dark" ]]; then
     feh_change_bg $HOME/.config/i3/dark-flower.jpg
-elif [[ "$1" == "light" ]]; then
+elif [[ $1 == "light" ]]; then
     feh_change_bg $HOME/.config/i3/pink-mountain.jpg
 fi
 
