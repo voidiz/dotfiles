3c00874979f131794bb95dc7dd77856cc2be4a99
voidiz <voidqt@hotmail.com>
2021-01-27 15:44:27 +0100

    update

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ i3lock-color polybar neovim-remote nnn-git xst-git lazygit
 otf-san-francisco otf-sfmono ttf-font-awesome 
 ttf-material-design-icons ttf-unifont
 
-chatterino2-git spicetify-cli spotify ffmpeg-compat-57
+chatterino2-git spicetify-cli spotify sp ffmpeg-compat-57
 dropbox mpv-mpris indicator-kdeconnect-git
 "
 
