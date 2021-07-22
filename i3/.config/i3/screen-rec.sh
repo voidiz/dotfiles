a81a1ec59c19481c446ee12098df5d93e3fb5bd8
voidiz <voidqt@hotmail.com>
2021-07-22 10:57:44 +0200

    change screen rec path

--- i3/.config/i3/screen-rec.sh
+++ i3/.config/i3/screen-rec.sh
@@ -4,7 +4,7 @@ set -eu
 
 delay=1
 duration=5
-filepath="$HOME/$(date +%s).mp4"
+filepath="$HOME/Videos/$(date +%s).mp4"
 
 giph -s -d $delay -t $duration -y $filepath
 dragon-drag-and-drop $filepath --and-exit
