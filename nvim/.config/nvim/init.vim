1eb128735d1215aad70ece26919f41192593e90c
voidiz <voidqt@hotmail.com>
2021-07-30 23:23:01 +0200

    update nvim

--- nvim/.config/nvim/init.vim
+++ nvim/.config/nvim/init.vim
@@ -166,9 +166,9 @@ au Filetype asm,go
 " au BufNewFile,BufRead *.js,*.html,*.css,*.php:
 au Filetype javascript,html,htmldjango,css,javascript.jsx,
             \typescriptreact,typescript
-    \ setl tabstop=2 |
-    \ setl softtabstop=2 |
-    \ setl shiftwidth=2 |
+    \ setl tabstop=4 |
+    \ setl softtabstop=4 |
+    \ setl shiftwidth=4 |
     \ setl fileformat=unix
 """""""""""""""""""""""""""""""""""""""""""
 
--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -1,7 +1,6 @@
 ----------------------------------------------
 -- Autopairs config
 require'nvim-autopairs'.setup {
-    check_ts = true,
     fast_wrap = {}
 }
 ----------------------------------------------
@@ -12,12 +11,6 @@ require'nvim-treesitter.configs'.setup {
     -- ensure_installed = { "c", "cpp", "python", "go", "json", "javascript" },
     ensure_installed = "maintained",
     highlight = {
-        enable = true
-    },
-    indent = {
-        enable = true
-    },
-    autopairs = {
         enable = true
     }
 }
@@ -151,30 +144,19 @@ local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
-
 -- Use (s-)tab to:
 --- move to prev/next item in completion menuone
 --- jump to prev/next snippet's placeholder
 _G.tab_complete = function()
     if vim.fn.pumvisible() == 1 then
         return t "<C-n>"
-    elseif vim.fn['vsnip#available'](1) == 1 then
-        return t "<Plug>(vsnip-expand-or-jump)"
-    elseif check_back_space() then
-        return t "<Tab>"
     else
-        return vim.fn['compe#complete']()
+        return t "<Tab>"
     end
 end
 _G.s_tab_complete = function()
     if vim.fn.pumvisible() == 1 then
         return t "<C-p>"
-    elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-        return t "<Plug>(vsnip-jump-prev)"
     else
         -- If <S-Tab> is not working in your terminal, change it to <C-h>
         return t "<S-Tab>"
