15ea39b5fd9034ea49701b03e0bbf5221df2dffd
voidiz <voidqt@hotmail.com>
2022-07-19 11:22:22 +0200

    fix screen rec

--- i3/.config/i3/screen-rec.sh
+++ i3/.config/i3/screen-rec.sh
@@ -3,8 +3,8 @@
 set -eu
 
 delay=1
-duration=15
+duration=10
 filepath="$HOME/Videos/$(date +%s).mp4"
 
 giph -s -d $delay -t $duration -y $filepath
-dragon-drag-and-drop $filepath --and-exit
+dragon-drop $filepath --and-exit
