14eadc97898caef1617f9b34e0ca0e2ce90b5c3f
voidiz <voidqt@hotmail.com>
2023-02-28 00:08:28 +0900

    telescope find files fallback

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,17 @@ local M = {
     dependencies = { "nvim-lua/plenary.nvim" },
 }
 
+-- Open git files, or fallback to find files if not in a git directory
+local function git_files_fallback()
+    local builtin = require("telescope.builtin")
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    if vim.v.shell_error == 0 then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
+end
+
 function M.config()
     local actions = require("telescope.actions")
     require("telescope").setup({
@@ -19,7 +30,7 @@ function M.config()
 
     local builtin = require("telescope.builtin")
 
-    vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+    vim.keymap.set("n", "<C-p>", git_files_fallback, {})
     vim.keymap.set("n", "<leader>fi", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fb", builtin.current_buffer_fuzzy_find, {})
     vim.keymap.set("n", "<leader>ff", builtin.resume, {})
