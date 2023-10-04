e84c3ec8435aaf9df7ed3b258e0785f2dcc73d85
voidiz <29259387+voidiz@users.noreply.github.com>
2023-10-04 10:28:04 +0200

    update indent-blankline

--- nvim/.config/nvim/lua/plugins/indentline.lua
+++ nvim/.config/nvim/lua/plugins/indentline.lua
@@ -1,7 +1,14 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     opts = {
-        show_current_context = true,
-        show_trailing_blankline_indent = false,
+        whitespace = {
+            remove_blankline_trail = false,
+        },
+        scope = {
+            show_start = false,
+            show_end = false,
+            highlight = { "Function" },
+        },
     },
 }
