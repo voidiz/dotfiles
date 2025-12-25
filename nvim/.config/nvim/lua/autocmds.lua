c419703e78e33dd573d402665ba03b313ab6f8d5
voidiz <29259387+voidiz@users.noreply.github.com>
2025-12-25 21:37:41 +0100

    config lsp after LspAttach event
    fixes clangd on_attach not being set

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -3,6 +3,47 @@ local autocmd = vim.api.nvim_create_autocmd
 
 local keymap_opts = { noremap = true, silent = true }
 
+---@param buffer integer
+local set_keymaps = function(buffer)
+    -- Mappings
+    local opts = { noremap = true, silent = true, buffer = buffer }
+    vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    vim.keymap.set("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    vim.keymap.set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+    vim.keymap.set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+    vim.keymap.set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+    vim.keymap.set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+    vim.keymap.set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+    vim.keymap.set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+    vim.keymap.set("n", "[g", "<cmd>lua vim.diagnostic.jump({ count = -1, float = true })<CR>", opts)
+    vim.keymap.set("n", "]g", "<cmd>lua vim.diagnostic.jump({ count = 1, float = true })<CR>", opts)
+    vim.keymap.set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+    vim.keymap.set("x", "<leader>fo", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+
+    -- Replaced by glance
+    -- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    -- vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    -- vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+
+    -- Replaced by conform
+    -- vim.keymap.set("n", "<M-S-F>", "<cmd>lua vim.lsp.buf.format{ async = true }<CR>", opts)
+end
+
+augroup("lsp", { clear = true })
+autocmd("LspAttach", {
+    group = "lsp",
+    callback = function(ev)
+        set_keymaps(ev.buf)
+
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        assert(client ~= nil)
+        client.server_capabilities =
+            vim.tbl_deep_extend("force", client.server_capabilities, require("cmp_nvim_lsp").default_capabilities())
+    end,
+})
+
 augroup("ft_python", { clear = true })
 autocmd("Filetype", {
     group = "ft_python",
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,40 +1,9 @@
-local on_attach = function(client, bufnr)
-    -- Mappings
-    local opts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    vim.keymap.set("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    vim.keymap.set("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    vim.keymap.set("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    vim.keymap.set("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    vim.keymap.set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    vim.keymap.set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    vim.keymap.set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    vim.keymap.set("n", "[g", "<cmd>lua vim.diagnostic.jump({ count = -1, float = true })<CR>", opts)
-    vim.keymap.set("n", "]g", "<cmd>lua vim.diagnostic.jump({ count = 1, float = true })<CR>", opts)
-    vim.keymap.set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    vim.keymap.set("x", "<leader>fo", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-
-    -- Replaced by glance
-    -- vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    -- vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    -- vim.keymap.set("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-
-    -- Replaced by conform
-    -- vim.keymap.set("n", "<M-S-F>", "<cmd>lua vim.lsp.buf.format{ async = true }<CR>", opts)
-end
-
-vim.lsp.config("*", {
-    on_attach = on_attach,
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
-})
-
 vim.lsp.enable("clangd")
 vim.lsp.enable("gopls")
 vim.lsp.enable("nixd")
 vim.lsp.enable("tailwindcss")
 vim.lsp.enable("vtsls")
+vim.lsp.enable("lua_ls")
 
 vim.diagnostic.config({
     virtual_text = true,
